d4693da54519bdc76a85d52e1e85057c91f27b6c
dantezy <zhangyet@gmail.com>
2021-12-15 10:33:53 +0800

    [feature]: move customize content to a gitignore file
    Signed-off-by: Dante Zhang <zhangyet@gmail.com>

--- init.el
+++ init.el
@@ -14,18 +14,5 @@
 (require 'basic)
 (require 'rc-org)
 
-;; auto generate
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-roam company auto-complete neotree neo-tree moe-theme use-package magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
