006190d68d506f67e1f337981913b3c4bc22af5e
Dantezy <zhangyet@gmail.com>
2025-02-12 10:34:03 +0800

    Add eglot
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- rc/rc-c.el
+++ rc/rc-c.el
@@ -7,6 +7,12 @@
 	  (lambda ()
 	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
 	      (ggtags-mode 1))))
+(use-package eglot
+  :ensure t)
+
+(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
 
 (provide 'rc-c)
 ;;;
